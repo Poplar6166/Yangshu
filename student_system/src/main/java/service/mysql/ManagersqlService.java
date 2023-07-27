970d82c3ad1f77839360c860ac74a8d7870fc6dc
yang <1657114904@qq.com>
2023-07-27 09:54:08 +0800

    message

--- student_system/src/main/java/service/mysql/ManagersqlService.java
+++ student_system/src/main/java/service/mysql/ManagersqlService.java
@@ -1,91 +1,96 @@
 package service.mysql;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import jwxt.Manager;
 import service.SuperManagersql;
+import util.JDBCTemplate;
 
+import javax.sql.DataSource;
+import javax.sql.rowset.JdbcRowSet;
 import java.sql.*;
 import java.util.*;
+import java.sql.Connection;
 
 public class ManagersqlService implements SuperManagersql {
-    String JDBC_URL = "jdbc:mysql://localhost:3306/test";
-    String JDBC_USER = "root";
-    String JDBC_PASSWORD = "root";
-    Map<Long,Manager> data = new HashMap<>();
-    public List<Manager> getAll(){
+    Map<Long, Manager> data = new HashMap<>();
+    public List<Manager> getAll() {
         List<Manager> result = new ArrayList<>();
-        try(Connection coon = DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD)){
-            try(PreparedStatement ps =  coon.prepareStatement("SELECT * FROM Manager")){
+        try (Connection coon = JDBCTemplate.getInstance()) {
+            try (PreparedStatement ps = coon.prepareStatement("SELECT * FROM Manager")) {
                 ResultSet rs = ps.executeQuery();
-                while(rs.next()){
+                while (rs.next()) {
                     Manager manager = new Manager();
                     long id = rs.getLong("managerID");
                     String name = rs.getString("managerName");
-                    manager.setManager(id,name);
+                    manager.setManager(id, name);
                     result.add(manager);
                 }
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return result;
     }
-    public void add(Manager manager){
-        try(Connection coon = DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD)){
-            try(PreparedStatement ps = coon.prepareStatement("INSERT INTO Manager VALUES (?,?)")){
-                ps.setObject(1,manager.getManagerId());
-                ps.setObject(1,manager.getManagerName());
+
+    public void add(Manager manager) {
+        try (Connection coon = JDBCTemplate.getInstance()) {
+            try (PreparedStatement ps = coon.prepareStatement("INSERT INTO Manager VALUES (?,?)")) {
+                ps.setObject(1, manager.getManagerId());
+                ps.setObject(2, manager.getManagerName());
                 int n = ps.executeUpdate();
-                if(n > 0)
+                if (n > 0)
                     System.out.println("添加成功!");
                 else
                     System.out.println("添加失败");
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public boolean delete(long id){
-        try(Connection coon = DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD)){
-            try(PreparedStatement ps = coon.prepareStatement("DELETE FROM Manager WHERE managerID = ?")){
-                ps.setObject(1,id);
+    public boolean delete(long id) {
+        try (Connection coon = JDBCTemplate.getInstance()) {
+            try (PreparedStatement ps = coon.prepareStatement("DELETE FROM Manager WHERE managerID = ?")) {
+                ps.setObject(1, id);
                 int n = ps.executeUpdate();
-                if(n > 0)
+                if (n > 0)
                     return true;
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return false;
     }
 
-    public boolean change(Manager manager){
-    try(Connection coon = DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD)){
-        try(PreparedStatement ps = coon.prepareStatement("UPDATE Manager SET managerName = ? WHERE managerID = ?")){
-            ps.setObject(1,manager.getManagerName());
-            ps.setObject(2,manager.getManagerId());
-            int n = ps.executeUpdate();
-            if(n > 0)
-                return true;
-        }
-    }catch (SQLException e){
-        throw new RuntimeException(e);
-    }
-    return false;
-    }
-    public Manager findManager(long id){
-        Manager manager = new Manager();
-        try(Connection coon = DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD)){
-            try(PreparedStatement ps = coon.prepareStatement("SELECT * FROM Manager WHERE managerID = ?")){
-                ps.setObject(1,id);
+    public boolean change(Manager manager) {
+        try (Connection coon = JDBCTemplate.getInstance()) {
+            try (PreparedStatement ps = coon.prepareStatement("UPDATE Manager SET managerName = ? WHERE managerID = ?")) {
+                ps.setObject(1, manager.getManagerName());
+                ps.setObject(2, manager.getManagerId());
                 int n = ps.executeUpdate();
-                if(n > 0){
+                if (n > 0)
+                    return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    public Manager findManager(long id) {
+        Manager manager = new Manager();
+        try (Connection coon = JDBCTemplate.getInstance()) {
+            try (PreparedStatement ps = coon.prepareStatement("SELECT * FROM Manager WHERE managerID = ?")) {
+                ps.setObject(1, id);
+                int n = ps.executeUpdate();
+                if (n > 0) {
                     ResultSet rs = ps.executeQuery();
-                    manager.setManager(rs.getLong("managerID"),rs.getString("managerName") );
+                    manager.setManager(rs.getLong("managerID"), rs.getString("managerName"));
                     return manager;
                 }
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return null;
---
+++ student_system/src/main/java/util/JDBCTemplate.java
@@ -0,0 +1,53 @@
+package util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JDBCTemplate {
+    private int count = 0;
+    // 静态字段引用唯一实例:
+    private static final JDBCTemplate INSTANCE = new JDBCTemplate();
+
+    HikariConfig config = new HikariConfig();
+    private DataSource ds;
+
+    public void setConfig() {
+        System.out.println("初始化连接池");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/test");
+        config.setUsername("root");
+        config.setPassword("root");
+        config.addDataSourceProperty("connectionTimeout", "1000");
+        config.addDataSourceProperty("idleTimeout", "60000");
+        config.addDataSourceProperty("maximumPoolSize", "10");
+        ds = new HikariDataSource(config);
+    }
+
+    // private构造方法保证外部无法实例化:
+    private JDBCTemplate() {
+    }
+
+    public DataSource getDataSource() {
+        System.out.println("获取连接");
+        if (INSTANCE.ds == null) {
+            INSTANCE.setConfig();
+        }
+        return INSTANCE.ds;
+    }
+
+    public static Connection getInstance() {
+        try {
+            return INSTANCE.getDataSource().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void print() {
+        System.out.println(++count);
+    }
+
+}
